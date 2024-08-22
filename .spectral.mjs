a973534ee0f70fb533697698a4c25615b4e16b87
prafulla.wadhai@smartbear.com <prafulla.wadhai@smartbear.com>
2024-08-22 12:57:54 +0530

    Added 2 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjI1NjM2MA/spectral.js?branch_slug=main&token=8ca27763-e53a-4d00-bcca-1b8f976cf674";
+export default { extends: ruleset };
